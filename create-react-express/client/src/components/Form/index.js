0ae7db74ef3a09449c542b05c71219f905f93d69
Remy Guts <remyguts@gmail.com>
2020-04-25 15:25:14 -0700

    styled and connected style sheet to form

--- create-react-express/client/src/components/Form/index.js
+++ create-react-express/client/src/components/Form/index.js
@@ -1,1 +1,32 @@
 "use strict";
+import React from "react";
+import "./style.css";
+
+const Form = (props) => {
+  return (
+    <form>
+      <div className="form-group">
+        <label className="BookSearchLabel">
+          <h3>Search for books</h3>
+        </label>
+        <input
+          className="col-12 form-control"
+          value={props.search}
+          type="text"
+          name="searchBook"
+          placeholder="Enter a book here"
+          onChange={props.handleInputChange}
+        />
+      </div>
+      <button
+        type="submit"
+        className="submitBtn btn btn-secondary"
+        onClick={props.handleFormSubmit}
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
