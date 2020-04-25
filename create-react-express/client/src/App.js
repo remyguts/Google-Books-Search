78b5b749646474665f5f1c0662dd3ecc2f1d6fc0
Remy Guts <remyguts@gmail.com>
2020-04-25 14:10:54 -0700

    adjusted some of the boiler plate code

---
+++ create-react-express/client/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SavedBooks from "./pages/SavedBooks";
+import SearchBooks from "./pages/SearchBooks";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={SearchBooks} />
+          <Route exact path="/saved" component={SavedBooks} />
+          <Route exact path="/saved/:id" component={SavedBooks} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
