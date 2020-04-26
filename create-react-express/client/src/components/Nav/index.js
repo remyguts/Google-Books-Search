9464ad02bcfc32565ceca38b872aed0c2c5481aa
Remy Guts <remyguts@gmail.com>
2020-04-26 16:47:57 -0700

    savebtm js

--- create-react-express/client/src/components/Nav/index.js
+++ create-react-express/client/src/components/Nav/index.js
@@ -1,1 +1,27 @@
-"use index";
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-info">
+      <a className="navbar-brand" href="/">
+        <h2 className="text-white">Bookshelf</h2>
+      </a>
+      <div id="navbar">
+        <ul className="nav justify-content-end">
+          <li className="nav-item" id="home">
+            <a className="nav-link text-white" href="/">
+              Home
+            </a>
+          </li>
+          <li className="nav-item" id="report">
+            <a className="nav-link text-white" href="/saved">
+              Favorites
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
