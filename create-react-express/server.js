def7455f8119b6fc8857d12af2f744f70db9b41b
Remy Guts <remyguts@gmail.com>
2020-04-26 19:31:02 -0700

    updated server.js file

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -1,26 +1,32 @@
-// loosely based on https://github.com/fullstackreact/food-lookup-demo
-
 const express = require(`express`);
-const path = require(`path`);
-const PORT = process.env.PORT || 3001;
+const mongoose = require(`mongoose`);
+const routes = require(`./routes`);
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === `production`) {
-    app.use(express.static(`client/build`));
+  app.use(express.static(`client/build`));
 }
 
-// Define API routes here
+// Add routes, both API and view
+app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get(`*`, (req, res) => {
-    res.sendFile(path.join(__dirname, `./client/build/index.html`));
-});
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || `mongodb://localhost/reactreadinglist`,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  }
+);
 
-app.listen(PORT, () => {
-    console.log(`API server up on http://localhost:${PORT}`);
-});
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`API Server up on http://localhost:${PORT}`)
+);
